0d5cf1077953a22c235c89f0012490b52f2ef54e
varun_bansal <varun_bansal@epam.com>
2024-09-24 16:29:00 +0530

    Selenium Tests

---
+++ src/test/java/org/epam/seleniumtests/SampleTests.java
@@ -0,0 +1,20 @@
+package org.epam.seleniumtests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SampleTests {
+
+    @Test
+    public void tests() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.quit();
+
+    }
+}
